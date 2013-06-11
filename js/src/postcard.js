d576f112fc1ab3febeeecf16668f7c2370552470
Doris Ip <dipdip0_0@hotmail.com>
2013-06-11 18:54:57 -0400

    add postcard msg

--- js/src/postcard.js
+++ js/src/postcard.js
@@ -8,11 +8,50 @@ require(["main"], function(common) {
             params.sName = "Memeri";
             params.msg = "Thanks for visiting."
         }
-        params.poem = "\"There's much to be seen.<br>There's much to experience.<br>There's never enough time.<br> To know what's out there.\"<br><p class='align-right'>~Toast</p>"
-        var text = "<h2>Hello {{rName}}!</h2><p>You've received a postcard from <b>{{sName}}</b>.  Hover over the postcard to reveal a special memeri.</p><p>{{sName}} has chosen to send you, |art name|."
-        //var text2 = "<div id='postcard-inside'><div id='center' class='column'>To: {{rName}}<br>From: {{sName}}</div><div id='left' class='column'>{{msg}}</div><div id='right' class='column'><img id='stamp' src='image/stamp1.png'></div></div>"
-        //var text2 = "<div id='postcard-inside' class='row'><div id='left' class='span5'><br><br>{{msg}}<br><br>From: {{sName}}</div><div class='span2' id='right'><div class='row pull-right'><div class='span2 pull-right stamp-img'><img id='stamp' src='image/stampToast.png'></div></div><div class='pull-left span3' id='to-header'>To: {{rName}}</div></div></div>"
-        var text2 = "<div id='postcard-inside' class='row-fluid'><div id='postcard-left' class='span6'><div id='postcard-msg'>{{rName}},<br>{{msg}}<br><p class='align-right'>-{{sName}}</p></div></div><div class='span6' id='postcard-right'><div class='row-fluid'><div class='span' id='stamp'><img id='stamp-img' src='{{stamp}}'></div></div><div class='row-fluid'><div id='poem' class='span12'>{{{poem}}}</div></div></div></div>"
+
+        mapping = {
+            "image/Peekaboo2S.jpg" : {
+                "name" : "Peekaboo",
+                "poem" : "I see you<br>Do you see me too?<br><p class='align-right'>~Toast</p>"
+            },
+            "image/scoop2S.jpg" : {
+                "name" : "Scoop",
+                "poem" : "One scoop<br>Two scoops<br>Three scoops<br>Forever more<br><p class='align-right'>~Toast</p>"
+            },
+            "image/shell-shock2S.jpg" : {
+                "name" : "Shell Shock",
+                "poem" : "Hey, you’re there, I’m here<br>We’re not that far apart<br>We look similar despite differences<br>Actually, I think we look quite good together<br>What do you say?<br><p class='align-right'>~D</p>"
+            },
+            "image/Snooze2S.jpg" : {
+                "name" : "Snooze",
+                "poem" : "Buzz<br>Snooze<br>Buzz<br>Snooze<br>Buzz<br>Is it summer yet?<br><p class='align-right'>~Toast</p>"
+            },
+            "image/Falling in time2S.jpg" : {
+                "name" : "Falling in time",
+                "poem" : "Time is precious<br>Time is money<br>Can I have more time?<br><p class='align-right'>~Toast</p>"
+            },
+            "image/BloomS.jpg" : {
+                "name" : "Bloom",
+                "poem" : "Roses are not red<br>Violets are not blue<br>I see a bud<br>Lookin' just like you<br><p class='align-right'>~Toast</p>"
+            },
+            "image/MakiEclipseS.jpg" : {
+                "name" : "Maki Eclipse",
+                "poem" : "All the pieces are put just right<br>Time to enjoy it with the eyes<br><br><p class='align-right'>~D</p>"
+            },
+            "image/Northpole2S.jpg" : {
+                "name" : "North Pole",
+                "poem" : "I see north<br>I see south<br>What you see is not what it seems<br>Then what is it that you see?<br><p class='align-right'>~Toast</p>"
+            },
+            "image/whatsoutthere2Small.jpg":{
+                "name": "What's Out There",
+                "poem": "\"There's much to be seen.<br>There's much to experience.<br>There's never enough time.<br> To know what's out there.\"<br><p class='align-right'>~Toast</p>"
+            }
+        }
+
+        params.poem = mapping[params.img].poem
+        params.img_name = mapping[params.img].name
+        var text = "<h2>Hello {{rName}}!</h2><p>You've received a postcard from <b>{{sName}}</b>.  Hover over the postcard to reveal a special memeri.</p><p>{{sName}} has chosen to send you, <b>{{img_name}}</b>."
+        var text2 = "<div id='postcard-inside' class='row-fluid'><div id='postcard-left' class='span6'><div id='postcard-msg'>{{rName}},<br>{{msg}}<br><p class='align-right'>-{{sName}}</p></div></div><div class='span6' id='postcard-right'><div class='row-fluid'><div class='span6 offset4' id='stamp'><img id='stamp-img' src='{{stamp}}'></div></div><div class='row-fluid'><div id='poem' class='span12'>{{{poem}}}</div></div></div></div>"
         var output = Mustache.to_html(text, params);
         var output2 = Mustache.to_html(text2, params);
 
