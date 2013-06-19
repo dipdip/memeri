a39c98900e1df84d6b36b0f8125bff1c76a37369
Doris Ip <dipdip0_0@hotmail.com>
2013-06-18 20:33:36 -0400

    comment out carousel code

--- js/src/home.js
+++ js/src/home.js
@@ -2,6 +2,6 @@ require(["main"], function(common) {
     require(["carousel", "navbar"], function(carousel, navbar) {
         // Render navbar
         navbar.addNavBar("home");
-        return carousel.show(['homeCarousel1', 'homeCarousel2', 'homeCarousel3']);
+        //return carousel.show(['homeCarousel1', 'homeCarousel2', 'homeCarousel3']);
     });
 });
