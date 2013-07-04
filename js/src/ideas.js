eef3c243f19d9cf5f3c31a56eecdfa306bb15ca1
Doris Ip <dipdip0_0@hotmail.com>
2013-07-04 18:39:09 -0400

    fix ideas

--- js/src/ideas.js
+++ js/src/ideas.js
@@ -1,7 +1,8 @@
 require(["main"], function(common) {
-    require(["carousel", "navbar"], function(carousel, navbar) {
+    require(["jquery", "carousel", "navbar"], function($, carousel, navbar) {
         // Render navbar
         navbar.addNavBar("ideas");
+        
         return carousel.show(["ideasCarousel1"]);
     });
 });
