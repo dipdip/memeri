71ba26949baf1c2d4c05a0eeef90879c3c647aed
dd <twong_dd@yahoo.com>
2014-08-16 14:47:01 -0400

    new stuff

--- public/js/src/home.js
+++
@@ -1,7 +0,0 @@
-require(["main"], function(common) {
-    require(["navbar"], function(navbar) {
-        // Render navbar
-        //navbar.addNavBar("home");
-        //return carousel.show(['homeCarousel1', 'homeCarousel2', 'homeCarousel3']);
-    });
-});
--- public/js/src/ideas.js
+++
@@ -1,8 +0,0 @@
-require(["main"], function(common) {
-    require(["jquery", "navbar","carousel"], function($, navbar, carousel) {
-        // Render navbar
-        //navbar.addNavBar("ideas");
-        
-        return carousel.show(["ideasCarousel1", "ideasCarousel2"]);
-    });
-});
